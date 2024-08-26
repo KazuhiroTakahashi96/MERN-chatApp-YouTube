0cd9045ae3c4dce36cf838dd986aba905fc61243
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-08-26 22:48:50 +0900

    import auth, DB modules

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  // root route http://localhost:5000/
+  // root route example:http://localhost:5000/
   res.send("Hello World!");
 });
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server is Running on port ${PORT}`);
 });
