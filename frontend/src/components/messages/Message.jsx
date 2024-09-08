7ce4e33c57f18499ff152be56c946618ad9cc1a7
Kazuhiro Takahashi <num96kazu19@icloud.com>
2024-09-08 15:05:46 +0900

    get messages

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -3,17 +3,17 @@ import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
-  // const { authUser } = useAuthContext();
-  // const { selectedConversation } = useConversation();
-  // const fromMe = message.senderId === authUser._id;
-  // const formattedTime = extractTime(message.createdAt);
-  // const chatClassName = fromMe ? "chat-end" : "chat-start";
-  // const profilePic = fromMe
-  //   ? authUser.profilePic
-  //   : selectedConversation?.profilePic;
-  // const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-  // const shakeClass = message.shouldShake ? "shake" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
 
   return (
     <div className={`chat ${chatClassName}`}>
@@ -25,7 +25,7 @@ const Message = ({ message }) => {
       <div
         className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
       >
-        {/* {message.message} */}
+        {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
         {formattedTime}
